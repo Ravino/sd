91b5b929c01a4d00b66aa2e59d03145eeffe136e
ravino <yatakayazvezda@yandex.ru>
2018-07-13 15:48:34 +1000

    Updated rules code nodejs for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   "env": {
+    "amd": true,
     "node": true,
     "commonjs": true,
     "es6": true
   },
   "extends": [
     "airbnb-base",
-    "node",
   ],
   "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "plugins": [
-      "import",
       "promise",
     ],
   "rules": {
@@ -26,10 +25,13 @@ module.exports = {
     "comma-dangle": ["error", "always"],
     "func-call-spacing": ["error", "always"],
     "padded-blocks": ["error", "always"],
+    "func-names": ["error", "as-needed"],
     "arrow-body-style": ["error", "as-needed", { "requireReturnForObjectLiteral": true }],
     "no-whitespace-before-property": "off",
     "global-require": "off",
     "no-spaced-func": "off",
     "no-new": "off",
+    "no-continue": "off",
+    "no-restricted-syntax": "off",
   }
 };
