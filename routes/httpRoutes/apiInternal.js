34a74226d738caccaa4d5efb6be413d3b8272f3c
ravino <yatakayazvezda@yandex.ru>
2018-07-30 07:20:58 +1000

    Added update in route apiInternal

--- routes/httpRoutes/apiInternal.js
+++ routes/httpRoutes/apiInternal.js
@@ -4,12 +4,16 @@ const router = require ("router") ();
 
 
 router. get ("/i", (req, res) => {
+
   res. send ("hello");
+
 });
 
 
 router. use ("*", (req, res) => {
+
   res. send (404);
+
 });
 
 
