4c3ddbfae3fb30ddfaf13abd63b534e10d1766e3
ravino <yatakayazvezda@yandex.ru>
2018-07-30 07:19:22 +1000

    Added update for handler register

--- routes/httpRoutes/apiInternal/register/handler.js
+++ routes/httpRoutes/apiInternal/register/handler.js
@@ -11,75 +11,79 @@ const addStr = "My love ";
 const pattern = /^[^\n\s\r]{1,}$/;
 
 
+module. exports = () => (req, res) => {
 
-module. exports = () => {
+  if (pattern. test (req. body. login) == false) {
 
-  return (req, res) => {
+    res. send ("Login incorrect");
+    return false;
 
-    if (pattern. test (req. body. login) == false) {
-      res. send ("Login incorrect");
-      return false;
+  }
+
+
+  if (pattern. test (req. body. password) == false) {
+
+    res. send ("Password incorrect");
+    return false;
+
+  }
+
+
+  if (req. body. password !== req. body. repeatPassword) {
+
+    res. send ("Password do not match");
+    return false;
+
+  }
+
+
+  if (pattern. test (req. body. name) == false) {
+
+    res. send ("Name incorrect");
+    return false;
+
+  }
+
+
+  let login = trim (req. body. login);
+  let password = trim (req. body. password);
+  let name = trim (req. body. name);
+
+
+  login = hsc (login);
+  password = hsc (password);
+  name = hsc (name);
+
+
+  const salt = crypto. createHash ("sha512"). update (`${login + Math. random ()}nekrasov.pw`). digest ("hex");
+  const hashPassword = crypto. createHash ("sha512"). update (`${password + salt}nekrasov.pw`). digest ("hex");
+  const saltHashPassword = crypto. createHash ("sha512"). update (`${hashPassword + salt}nekrasov.pw`). digest ("hex");
+  const strUuid = uuidV4 ();
+  const userUuid = uuidV5 (addStr, strUuid);
+  const namePage = `/${login}`;
+
+
+  const insertUserData = [
+    userUuid,
+    login,
+    saltHashPassword, salt,
+    name,
+    namePage,
+  ];
+
+
+  pg. query ("select insertUser ($1::uuid, $2::varchar, $3::varchar, $4::varchar, $5::varchar, $6::varchar)", insertUserData). then ((statusInsertUserData) => {
+
+    if (statusInsertUserData. row [0] || statusInsertUserData. row [0]. insertuser == 1) {
+
+      res. redirect ("/");
+      return true;
+
     }
 
+    res. redirect ("/error");
+    return false;
 
-    if (pattern. test (req. body. password) == false) {
-      res. send ("Password incorrect");
-      return false;
-    }
+  });
 
-
-    if (req. body. password !== req. body. repeatPassword) {
-      res. send ("Password do not match");
-      return false;
-    }
-
-
-    if (pattern. test (req. body. name) == false) {
-      res. send ("Name incorrect");
-      return false;
-    }
-
-
-
-    let login = trim (req. body. login);
-    let password = trim (req. body. password);
-    let name = trim (req. body. name);
-
-
-    login = hsc (login);
-    password = hsc (password);
-    name = hsc (name);
-
-
-
-    const salt = crypto. createHash ("sha512"). update (login + Math. random () + "nekrasov.pw"). digest ("hex");
-    const hashPassword = crypto. createHash ("sha512"). update (password + salt + "nekrasov.pw"). digest ("hex");
-    const saltHashPassword = crypto. createHash ("sha512"). update (hashPassword + salt + "nekrasov.pw"). digest ("hex");
-    const strUuid = uuidV4 ();
-    const userUuid = uuidV5 (addStr, strUuid);
-    const namePage = "/" + login;
-
-
-    const insertUserData = [
-      userUuid,
-      login,
-      saltHashPassword, salt,
-      name,
-      namePage,
-    ];
-
-
-
-    pg. query ("select insertUser ($1::uuid, $2::varchar, $3::varchar, $4::varchar, $5::varchar, $6::varchar)", insertUserData). then ( statusInsertUserData => {
-
-      if (statusInsertUserData. row [0] || statusInsertUserData. row [0]. insertuser == 1) {
-        res. redirect ("/");
-        return true;
-      }
-
-      res. redirect ("/error");
-      return false;
-
-    });
-  };
 };
