bc18412304b901a3459e1df72b94536347193159
ravino <yatakayazvezda@yandex.ru>
2018-07-30 07:18:39 +1000

    Added new path local for login route.

---
+++ routes/httpRoutes/public/loginLocal.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const router = require ("router") ();
+
+
+
+router. get ("/local", (req, res) => {
+
+  if (req. isAuthenticated ()) {
+    res. redirect ("/");
+    return true;
+
+  }
+
+
+  res. render ("/loginLocal.pug");
+  return false;
+
+});
+
+
+
+module. exports = () => {
+  return router;
+};
