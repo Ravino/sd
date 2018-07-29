a6b718298a84d95c180ec8765bbf0e51e1ab300a
ravino <yatakayazvezda@yandex.ru>
2018-07-30 07:15:59 +1000

    Added new route for local login

--- routes/httpRoutes/public.js
+++ routes/httpRoutes/public.js
@@ -6,28 +6,35 @@ const router = require ("router") ();
 router. get ("/", (req, res) => {
 
   if (req. isAuthenticated ()) {
+
     res. redirect ("/");
     return true;
+
   }
 
 
   res. render ("index.pug");
   return false;
-});
 
+});
 
 
 router. get ("/register", (req, res) => {
 
   if (req. isAuthenticated ()) {
+
     res. redirect ("/");
     return true;
+
   }
 
 
   res. render ("register.pug");
   return false;
+
 });
 
 
+
+router. use ("/login", require ("./public/loginLocal.js") ());
 module. exports = () => router;
