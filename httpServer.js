cf6b1e620ac793653ac5ddf5d7336209f930c6f2
ravino <yatakayazvezda@yandex.ru>
2018-08-02 21:30:26 +1000

    Added and configurated logger console-log-level with added in object global

--- httpServer.js
+++ httpServer.js
@@ -2,12 +2,16 @@
 
 
 const app = require ("express") ();
+const logger = require ("console-log-level");
 
 const httpConfig = require ("./configs/httpConfig.js") (app);
 const httpRoutes = require ("./routes/httpRoutes.js") (app);
 
 const pg = require ("./connectsPool/pg.js") ();
 const httpPassport = require ("./authenticates/httpPassport.js") ();
+
+
+global. logger = logger ({ "prefix": level => `${level}:${new Date (). toISOString ()}:`, });
 
 
 httpConfig. settings ();
@@ -20,12 +24,16 @@ pg. connect (). then ((db) => {
   global. pg = db;
   global. httpPassport = httpPassport;
 
-  app. listen (3000);
+  app. listen (3000, () => {
+
+    global. logger. info ("Start server running on port 3000");
+
+  });
 
 },
 
 (err) => {
 
-  console. log (err);
+  global. logger. error (err);
 
 });
