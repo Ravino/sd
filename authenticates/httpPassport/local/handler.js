22e50fc2dd726ddf7296a5d1da87fa67e0811898
ravino <yatakayazvezda@yandex.ru>
2018-08-02 21:33:24 +1000

    changed in accordance with the rules codestyle

--- authenticates/httpPassport/local/handler.js
+++ authenticates/httpPassport/local/handler.js
@@ -9,60 +9,81 @@ const trim = require ("trim");
 const pattern = /^[^\n\r\s]$/;
 
 
+module. exports = () => (pReq, pLogin, pPassword, pDone) => {
 
-module. exports = () => {
+//  const req = pReq;
+  let login = pLogin;
+  let password = pPassword;
+  const done = pDone;
 
-  return (req, login, password, done) => {
 
-    if (pattern. test (login) == false) {
-      done (null, false, {"error": "incorrect login" });
+  if (pattern. test (login) === false) {
+
+    done (null, false, { "error": "incorrect login", });
+    return false;
+
+  }
+
+
+  if (pattern. test (password) === false) {
+
+    done (null, false, { "error": "incorrect password", });
+    return false;
+
+  }
+
+
+  login = trim (login);
+  password = trim (password);
+
+  login = hsc (login);
+  password = hsc (password);
+
+
+  const transmitData = [
+    login,
+  ];
+
+
+  global. pg. query ("select * from users where login = $1::varchar", transmitData). then ((resultQueryUser) => {
+
+    if (!resultQueryUser. row [0]) {
+
+      done (null, false, { "error": "incorrect data", });
       return false;
+
     }
 
 
-    if (pattern. test (password) == false) {
-      done (null, false, { "error": "incorrect password" });
+    const hashSalt = resultQueryUser. row [0]. salt;
+    const referenceHashSaltPassword = resultQueryUser. row [0]. password;
+
+    const hashPassword = crypto. createHash ("sha512"). update (`${password + hashSalt}nekrasov.pw`). digest ("hex");
+    const hashSaltPassword = crypto. createHash ("sha512"). update (`${hashPassword + hashSalt}nekrasov.pw`). digest ("hex");
+
+
+    if (referenceHashSaltPassword !== hashSaltPassword) {
+
+      done (null, false, { "error": "incorrect data", });
       return false;
+
     }
 
+    done (null, hashSaltPassword. row [0]);
+    return true;
 
-    login = trim (login);
-    password = trim (password);
-
-    login = hsc (login);
-    password = hsc (password);
+  },
 
 
-    pg. query ("select * from users where login = $1::varchar", [login]). then ( resultQueryUser => {
+  (err) => {
 
-      if (!resultQueryUser. row [0]) {
-        done (null, false, { "error": "incorrect data" });
-        return false;
-      }
+    global. logger. error (err);
 
+    done (err);
+    return false;
 
-      const hashSalt = resultQueryUser. row [0]. salt;
-      const referenceHashSaltPassword = resultQueryUser. row [0]. password;
-
-      const hashPassword = crypto. createHash ("sha512"). update (password + hashSalt + "nekrasov.pw"). digest ("hex");
-      const hashSaltPassword = crypto. createHash ("sha512"). update (hashPassword + hashSaltPassword + "nekrasov.pw"). digest ("hex");
-
-
-      if (referenceHashSaltPassword !== hashSaltPassword) {
-        done (null, false, { "error": "incorrect data" {);
-        return false;
-      }
-
-      done (null, hashSaltPassword. row [0]);
-      return true;
-    },
-
-
-    err => {
-      console. log (err);
-
-      done (err);
-      return false;
-    });
   });
+
+  return undefined;
+
 };
