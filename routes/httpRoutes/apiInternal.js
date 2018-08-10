0802971830164096a5e0e23c5899f33b8784e1b3
ravino <yatakayazvezda@yandex.ru>
2018-08-10 10:17:30 -0400

    Added route login

--- routes/httpRoutes/apiInternal.js
+++ routes/httpRoutes/apiInternal.js
@@ -4,6 +4,7 @@ const router = require ("router") ();
 
 
 router. use ("/register", require ("./apiInternal/register.js") ());
+router. use ("/login", require ("./apiInternal/login.js") ());
 
 
 module. exports = () => router;
