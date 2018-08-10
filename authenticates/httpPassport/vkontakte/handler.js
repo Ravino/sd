c9eec713302838d8f80839cc3cd0d73dde589035
ravino <yatakayazvezda@yandex.ru>
2018-08-10 10:24:12 -0400

    Added examples handling auth vkontakte

--- authenticates/httpPassport/vkontakte/handler.js
+++ authenticates/httpPassport/vkontakte/handler.js
@@ -1,3 +1,12 @@
 "use strict";
 
-module. exports = () => (/* pReq, pAccess, pRefresh, pProfile, pParam, pDone */) => undefined;
+module. exports = () => (pReq, pAccess, pRefresh, pProfile, pParam, pDone) => {
+
+  global. logger. info (pReq);
+  global. logger. info (pAccess);
+  global. logger. info (pRefresh);
+  global. logger. info (pParam);
+  global. logger. info (pProfile);
+  pDone ();
+
+};
