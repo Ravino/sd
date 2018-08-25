063390e63ad76a4acd8497939cdd47bd468d8344
ravino <yatakayazvezda@yandex.ru>
2018-08-24 21:43:44 -0400

    Added new path for login to local

--- routes/httpRoutes/apiInternal/login.js
+++ routes/httpRoutes/apiInternal/login.js
@@ -3,6 +3,8 @@
 const router = require ("router") ();
 
 
+router. post ("/local", global. passport. authenticate ("local", { "successRedirect": "/", "failureRedirect": "/", "failureFlash": true, }));
+
 router. get ("/vkontakte", global. passport. authenticate ("vkontakte", { "successRedirect": "/", "failureRedirect": "/api/internal/error/vkontakte", }));
 
 
