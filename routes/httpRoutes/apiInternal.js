f1b264bb59c7cefc6240e61874a438ff6aff8127
ravino <yatakayazvezda@yandex.ru>
2018-08-25 22:20:56 -0400

    Added route logout destroy session

--- routes/httpRoutes/apiInternal.js
+++ routes/httpRoutes/apiInternal.js
@@ -4,7 +4,10 @@ const router = require ("router") ();
 
 
 router. use ("/register", require ("./apiInternal/register.js") ());
+
 router. use ("/login", require ("./apiInternal/login.js") ());
+
+router. use ("/logout", require ("./apiInternal/logout.js") ());
 
 
 module. exports = () => router;
